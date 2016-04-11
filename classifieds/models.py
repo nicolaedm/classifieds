0c0a2ea69255e947b6eec5d1d990be6dced2ea77
Nicolae Dorin Mirca <nicolaedm88@gmail.com>
2016-04-11 19:48:07 +0300

    create models

--- classifieds/models.py
+++ classifieds/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.category_title
+
+
+class Add(models.Model):
+    pub_date = models.DateTimeField('date published')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    add_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.add_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'classifieds.apps.ClassifiedsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
